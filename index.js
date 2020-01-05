14d631eadeedb5213e840cd35fa8b3ecad9192c9
Grace Shih <vocalise@gmail.com>
2020-01-05 20:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  const newDriver = driver;
-  delete newDriver[key];
-  return newDriver;
+  const newDriver = Object.assign({}, driver);
+  delete newDriver.key;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
