36eddabf64b06126492d9e1001e5d03a8a2da4b6
Grace Shih <vocalise@gmail.com>
2020-01-05 20:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newObj = Object.assign({}, driver)
+  const newObj = Object.assign({}, driver);
   newObj[key] = value;
   return newObj;
 }
