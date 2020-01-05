b6681b7ec9bc4abab6459c200990489b7682b2e6
Grace Shih <vocalise@gmail.com>
2020-01-05 20:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your solution in this file!
 function updateDriverWithKeyAndValue(driver, key, value) {
-  
+  const newObj = driver;
+  newObj[key] = value;
+  return newObj;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
